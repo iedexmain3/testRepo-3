ed251894effb17b74159c8cbe5d44561cd153909
iedexmain3 <userwm2u@some.mail>
2017-03-25 21:20:29 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1490388344088
+//first_change1490474744232
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
