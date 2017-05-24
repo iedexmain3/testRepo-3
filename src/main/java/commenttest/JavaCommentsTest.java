db14c1993d72baa5d8aed733c20dcad6a5de38cb
iedexmain3 <usertdb5@some.mail>
2017-05-24 23:33:58 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1495571772431
+//first_change1495664596802
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
