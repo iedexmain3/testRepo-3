d834607922aa30d80db2261b10c0935486b6af55
iedexmain3 <userxkworm@some.mail>
2017-10-12 11:55:11 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1507750314886
+//first_change1507806525621
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
