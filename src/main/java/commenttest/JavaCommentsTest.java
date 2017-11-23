78bfff3069aea8cb15ebddae2d837d25f4c158c0
iedexmain3 <userad3vim@some.mail>
2017-11-23 00:16:47 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1511296287624
+//first_change1511393195556
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
